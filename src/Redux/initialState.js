478d9053907f11d740b3c982f7a3df30d80154cc
Serhii Sapiannyk <serhii09@icloud.com>
2022-02-19 17:49:51 +0100

    add MainPage /19.4

--- src/Redux/initialState.js
+++ src/Redux/initialState.js
@@ -1,7 +1,41 @@
 const initialState ={
 
   posts: [
-    
+    {
+      id: '1',
+      title: 'Main Article title',
+      shortDescription: 'Short description of the article...',
+      content: 'Main content of the article',
+      publishedDate: '02-02-2022',
+      author: 'John Doe'
+    },
+
+    {
+      id: '2',
+      title: 'First Article title',
+      shortDescription: 'Short description of the article...',
+      content: 'First content of the article',
+      publishedDate: '02-02-2022',
+      author: 'John Doe'
+    },
+
+    {
+      id: '3',
+      title: 'Second Article title',
+      shortDescription: 'Short description of the article...',
+      content: 'Second content of the article',
+      publishedDate: '02-02-2022',
+      author: 'John Doe'
+    },
+
+    {
+      id: '4',
+      title: 'Third Article title',
+      shortDescription: 'Short description of the article...',
+      content: 'Third Main content of the article',
+      publishedDate: '02-02-2022',
+      author: 'John Doe'
+    }
   ]
 }
 
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,9 @@
+import Posts from "../features/Posts"
 
 
 const Home = () => {
   return (
-    <h2>Home</h2>
+    <Posts/>
   )
 }
 
